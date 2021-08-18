7e9e95b5a3dcf09dc44d9a85d566ba1072347eb7
Korey Andrus <korey86@gmail.com>
2021-08-18 12:56:04 -0500

    refactor components and hooks

--- part1/counter/src/App.js
+++ part1/counter/src/App.js
@@ -1,23 +1,49 @@
 import React, { useState } from "react";
 
+const History = (props) => {
+  if(props.allClicks.length === 0) {
+    return (
+      <div>
+        The app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      Button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
-  // const [left, setLeft] = useState(0)
-  // const [right, setRight] = useState(0)
-  const [clicks, setClicks] = useState({
-    left: 0, right:0
-  })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 })
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
 
-  const handleRightClick = () => setClicks({ ...clicks, right: clicks.right + 1 })
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
   
 
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
